2901a3aa2edd9bc77976070c40a215be147aa8d4
pafliasT <paflias13@gmail.com>
2024-02-22 13:55:18 +0200

    fix nav component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Navigation from "./components/Navbar";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
